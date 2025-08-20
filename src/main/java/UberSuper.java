bbb21ccf0959094b038c54e95208b9296ee92565
Jie Yang <ngjieyang@hotmail.com>
2025-08-20 19:59:39 +0800

    added todo, deadline, event (Level 4)

--- src/main/java/UberSuper.java
+++ src/main/java/UberSuper.java
@@ -29,24 +29,62 @@ public class UberSuper {
 
     private static void add(Task task) {
         inputList.add(task);
-    }
-
-    private static void mark(int i) {
-        Task t = inputList.get(i);
-        t.mark();
+        String message = String.format("You now have %d tasks in the list \n", inputList.size());
         printLine();
-        System.out.print("Nice! I've marked this task as done: \n");
-        System.out.print(t + "\n");
+        System.out.print("Got it! I've added this task:\n" + task + "\n" + message);
         printLine();
     }
 
-    private static void unmark(int i) {
-        Task t = inputList.get(i);
-        t.unmark();
-        printLine();
-        System.out.print("Ok, I've marked this task as not done yet: \n");
-        System.out.print(t + "\n");
-        printLine();
+    private static void mark(String input) {
+        String[] parts = input.split("\\s+", 2);
+        int i = Integer.parseInt(parts[1]);
+        try {
+            Task t = inputList.get(i - 1);
+            t.mark();
+            printLine();
+            System.out.print("Nice! I've marked this task as done: \n");
+            System.out.print(t + "\n");
+            printLine();
+        } catch (Exception e) {
+            System.out.print("There's no such item \n");
+        }
+
+    }
+
+    private static void unmark(String input) {
+        String[] parts = input.split("\\s+", 2);
+        int i = Integer.parseInt(parts[1]);
+        try{
+            Task t = inputList.get(i - 1);
+            t.unmark();
+            printLine();
+            System.out.print("Ok, I've marked this task as not done yet: \n");
+            System.out.print(t + "\n");
+            printLine();
+        } catch (Exception e) {
+            System.out.print("There's no such task in the list \n");
+        }
+
+    }
+
+    private static void todo(String input) {
+        String[] parts = input.split("\\s+", 2);
+        add(new Todo(parts[1]));
+    }
+
+    private static void deadline(String input) {
+        String[] parts = input.split("/");
+        String desc = parts[0].replaceFirst("deadline", "").trim();
+        String dl = parts[1].replaceFirst("by", "(by:");
+        add(new Deadline(desc, dl + ")"));
+    }
+
+    private static void event(String input) {
+        String[] parts = input.split("/");
+        String desc = parts[0].replaceFirst("event", "").trim();
+        String startTime = parts[1].replaceFirst("from", "(from:");
+        String endTime = parts[2].replaceFirst("to", "to:");
+        add(new Event(desc, startTime, endTime + ")"));
     }
 
     private static void list(){
@@ -68,17 +106,18 @@ public class UberSuper {
             } else if (input.equals("list")) {
                 list();
             } else if (input.startsWith("mark ")) {
-                String[] parts = input.split("\\s+", 2);
-                int i = Integer.parseInt(parts[1]);
-                mark(i);
+                mark(input);
             } else if (input.startsWith("unmark ")) {
-                String[] parts = input.split("\\s+", 2);
-                int i = Integer.parseInt(parts[1]);
-                unmark(i);
+                unmark(input);
+            } else if (input.startsWith("todo ")){
+                todo(input);
+            } else if (input.startsWith("deadline")) {
+                deadline(input);
+            } else if (input.startsWith("event")) {
+                event(input);
             } else {
-                add(new Task(input));
                 printLine();
-                System.out.print("added: " + input + "\n");
+                System.out.print(input + "\n");
                 printLine();
             }
         }
@@ -88,17 +127,56 @@ public class UberSuper {
         private boolean isDone = false;
         private final String description;
 
-        public Task(String description){
+        public Task(String description) {
             this.description = description;
 
         }
         public void mark() {this.isDone = true;}
         public void unmark() {this.isDone = false;}
-
+        public boolean done() {return isDone;}
+        public String desc() {return description;}
         @Override
-        public String toString(){
+        public String toString() {
             return String.format("[%s] %s", isDone ? "X" : "", description);
         }
     }
 
+    private static class Todo extends Task {
+        public Todo(String description) {
+            super(description);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("[T][%s] %s", done() ? "X" : "", desc());
+        }
+
+    }
+
+    private static class Deadline extends Task {
+        private final String deadLine;
+        public Deadline(String description, String deadLine) {
+            super(description);
+            this.deadLine = deadLine;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("[D][%s] %s %s", done() ? "X" : "", desc(), this.deadLine);
+        }
+    }
+    private static class Event extends Task {
+        private final String startTime;
+        private final String endTime;
+        public Event(String description, String startTime, String endTime) {
+            super(description);
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("[E][%s] %s %s %s", done() ? "X" : "", desc(), this.startTime, this.endTime);
+        }
+    }
 }
