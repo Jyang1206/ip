5a3d2a80084298217d8f8402cae2b2f850aaa928
Jie Yang <ngjieyang@hotmail.com>
2025-08-20 17:35:35 +0800

    added mark and unmark (Level 3)

--- src/main/java/UberSuper.java
+++ src/main/java/UberSuper.java
@@ -5,7 +5,7 @@ public class UberSuper {
     private static final String botName = "UberSuper";
     private static final String line = "_________________________________";
     private static final Scanner sc = new Scanner(System.in);
-    private static ArrayList<String> inputList = new ArrayList<String>(100);
+    private static ArrayList<Task> inputList = new ArrayList<Task>(100);
 
     public static void main(String[] args) {
         greet();
@@ -26,26 +26,78 @@ public class UberSuper {
         System.out.print("Bye. Hope to see you again soon! \n");
         printLine();
     }
-    private static void echo() {
+
+    private static void add(Task task) {
+        inputList.add(task);
+    }
+
+    private static void mark(int i) {
+        Task t = inputList.get(i);
+        t.mark();
+        printLine();
+        System.out.print("Nice! I've marked this task as done: \n");
+        System.out.print(t + "\n");
+        printLine();
+    }
+
+    private static void unmark(int i) {
+        Task t = inputList.get(i);
+        t.unmark();
+        printLine();
+        System.out.print("Ok, I've marked this task as not done yet: \n");
+        System.out.print(t + "\n");
+        printLine();
+    }
+
+    private static void list(){
+        printLine();
         int listNum = 1;
+        System.out.print("Here are the tasks in your list:\n");
+        for (Task task : inputList) {
+            System.out.print(listNum + ". " + task + "\n");
+            listNum++;
+        }
+        printLine();
+    }
+    private static void echo() {
         while (sc.hasNextLine()) {
             String input = sc.nextLine().trim();
             if (input.equals("bye")) {
                 goodBye();
                 break;
             } else if (input.equals("list")) {
-                printLine();
-                for (String s : inputList) {
-                    System.out.print(s);
-                }
-                printLine();
+                list();
+            } else if (input.startsWith("mark ")) {
+                String[] parts = input.split("\\s+", 2);
+                int i = Integer.parseInt(parts[1]);
+                mark(i);
+            } else if (input.startsWith("unmark ")) {
+                String[] parts = input.split("\\s+", 2);
+                int i = Integer.parseInt(parts[1]);
+                unmark(i);
             } else {
-                inputList.add(listNum + ". " + input + "\n");
-                listNum++;
+                add(new Task(input));
                 printLine();
                 System.out.print("added: " + input + "\n");
                 printLine();
             }
+        }
+    }
+
+    private static class Task {
+        private boolean isDone = false;
+        private final String description;
+
+        public Task(String description){
+            this.description = description;
+
+        }
+        public void mark() {this.isDone = true;}
+        public void unmark() {this.isDone = false;}
+
+        @Override
+        public String toString(){
+            return String.format("[%s] %s", isDone ? "X" : "", description);
         }
     }
 
