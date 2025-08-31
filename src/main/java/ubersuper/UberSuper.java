09fb270da82a4fd73e8dec10a8ad1d7e365cb8e3
Jie Yang <ngjieyang@hotmail.com>
2025-08-31 16:49:27 +0800

    Add header comments to UberSuper class

--- src/main/java/ubersuper/UberSuper.java
+++ src/main/java/ubersuper/UberSuper.java
@@ -5,7 +5,12 @@ import ubersuper.utils.DataStorage;
 import ubersuper.utils.LoadedResult;
 import ubersuper.utils.Ui;
 import ubersuper.tasks.TaskList;
-
+/**
+ * Entry point of the UberSuper application.
+ * <p>
+ * Starts up by loading tasks from disk, greeting the user, and entering the
+ * command loop handled by {@link ubersuper.utils.Ui#echo()}.
+ */
 
 public class UberSuper {
     private final Scanner sc = new Scanner(System.in);
@@ -14,11 +19,21 @@ public class UberSuper {
     private final TaskList taskList = result.tasks();
     private final Ui ui = new Ui(sc, taskList);
 
+    /**
+     * Runs the application: shows the greeting and processes user commands
+     * until the {@code bye} command is entered.
+     */
 
     public void run() {
         ui.greet(result);
         ui.echo();
     }
+
+    /**
+     * Launches UberSuper.
+     *
+     * @param args command-line arguments (unused)
+     */
 
     public static void main(String[] args) {
         new UberSuper().run();
