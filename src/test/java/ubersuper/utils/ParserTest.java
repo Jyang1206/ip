f4b7c43fe8965b22e5ec5d5a638ac7b063141d38
Jie Yang <E1408891@u.nus.edu>
2025-09-10 19:49:02 +0800

    Add basic commands for client

--- src/test/java/ubersuper/utils/ParserTest.java
+++ src/test/java/ubersuper/utils/ParserTest.java
@@ -16,7 +16,7 @@ public class ParserTest {
     void fromInput_matchesHeadTokenCaseInsensitive_returnsCorrectKeyword() {
         assertEquals(CommandType.BYE, Parser.fromInput("bye"));
         assertEquals(CommandType.BYE, Parser.fromInput("Bye"));
-        assertEquals(CommandType.LIST, Parser.fromInput("list   "));
+        assertEquals(CommandType.TASKLIST, Parser.fromInput("list   "));
     }
     
     @Test
@@ -33,49 +33,49 @@ public class ParserTest {
     }
     @Test
     @DisplayName("ISO date only -> midnight")
-    void parseWhen_withIsoDate_returnsStartOfDay() throws UberExceptions {
-        LocalDateTime dt = Parser.parseWhen("2019-12-02");
+    void parseDateTime_withIsoDate_returnsStartOfDay() throws UberExceptions {
+        LocalDateTime dt = Parser.parseDateTime("2019-12-02");
         assertEquals(LocalDate.of(2019, 12, 2).atStartOfDay(), dt);
     }
 
     @Test
     @DisplayName("ISO date-time with 'T'")
-    void parseWhen_withIsoDateTimeWithT_returnsExactTime() throws UberExceptions {
-        LocalDateTime dt = Parser.parseWhen("2019-12-02T18:00");
+    void parseDateTime_withIsoDateTimeWithT_returnsExactTime() throws UberExceptions {
+        LocalDateTime dt = Parser.parseDateTime("2019-12-02T18:00");
         assertEquals(LocalDateTime.of(2019, 12, 2, 18, 0), dt);
     }
 
     @Test
     @DisplayName("ISO date-time with space")
-    void parseWhen_withIsoDateTimeWithSpace_returnsExactTime() throws UberExceptions {
-        LocalDateTime dt = Parser.parseWhen("2019-12-02 18:00");
+    void parseDateTime_withIsoDateTimeWithSpace_returnsExactTime() throws UberExceptions {
+        LocalDateTime dt = Parser.parseDateTime("2019-12-02 18:00");
         assertEquals(LocalDateTime.of(2019, 12, 2, 18, 0), dt);
     }
 
     @Test
     @DisplayName("d/M/uuuu HHmm and d/M/uuuu")
-    void parseWhen_withSlashDateAndHHmm_returnsParsed() throws UberExceptions {
+    void parseDateTime_withSlashDateAndHHmm_returnsParsed() throws UberExceptions {
         assertEquals(LocalDateTime.of(2019, 12, 2, 18, 0),
-                Parser.parseWhen("2/12/2019 1800"));
+                Parser.parseDateTime("2/12/2019 1800"));
         assertEquals(LocalDate.of(2019, 12, 2).atStartOfDay(),
-                Parser.parseWhen("2/12/2019"));
+                Parser.parseDateTime("2/12/2019"));
     }
 
     @Test
     @DisplayName("d-M-uuuu HHmm and d-M-uuuu")
-    void parseWhen_withDashDateAndHHmm_returnsParsed() throws UberExceptions {
+    void parseDateTime_withDashDateAndHHmm_returnsParsed() throws UberExceptions {
         assertEquals(LocalDateTime.of(2019, 12, 2, 18, 0),
-                Parser.parseWhen("2-12-2019 1800"));
+                Parser.parseDateTime("2-12-2019 1800"));
         assertEquals(LocalDate.of(2019, 12, 2).atStartOfDay(),
-                Parser.parseWhen("2-12-2019"));
+                Parser.parseDateTime("2-12-2019"));
     }
 
     @Test
     @DisplayName("Rejects invalid inputs")
-    void parseWhen_invalidInputs_throwsUberExceptionsid_inputs() {
-        assertThrows(UberExceptions.class, () -> Parser.parseWhen("not-a-date"));
-        assertThrows(UberExceptions.class, () -> Parser.parseWhen("10-10-20"));  // 2-digit year not supported
-        assertThrows(UberExceptions.class, () -> Parser.parseWhen("2019/12/02")); // unsupported pattern
+    void parseDateTime_invalidInputs_throwsUberExceptionsid_inputs() {
+        assertThrows(UberExceptions.class, () -> Parser.parseDateTime("not-a-date"));
+        assertThrows(UberExceptions.class, () -> Parser.parseDateTime("10-10-20"));  // 2-digit year not supported
+        assertThrows(UberExceptions.class, () -> Parser.parseDateTime("2019/12/02")); // unsupported pattern
     }
 
 }
